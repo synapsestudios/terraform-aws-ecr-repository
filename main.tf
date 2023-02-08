e2e59d85739d6bc78ca564fff13ef34ea424640f
Devin Lauderdale <devin.lauderdale@synapsestudios.com>
2023-02-08 17:20:39 -0600

    fix provider to required providers block

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
-provider "aws" {
-  region = var.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
 }
 
 resource "aws_ecr_repository" "this" {
